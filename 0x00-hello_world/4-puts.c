79bcffa8dfa7542b51b8fb607fa923f245e6c30c
anouarbelouafi <anwarbc1992@gmail.com>
2023-03-16 12:40:27 -0700

    1-compiler

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+   * main - print to screen
+   *
+   * Return: zero
+   */
+int main(void)
+{
+char message[] = "\"Programming is like building a multilingual puzzle";
+puts(message);
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+   * main - print to screen using printf
+   *
+   * Return: zero
+   */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
